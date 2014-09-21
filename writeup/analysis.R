b0b5bd2994532daad3d86767f8f6f07f63e4875d
Andy Petrella <andy.petrella@gmail.com>
2014-09-21 19:54:38 +0200

    with solce

--- writeup/analysis.R
+++ writeup/analysis.R
@@ -37,6 +37,7 @@ training <- training[, -1]
 with.nas <- sort(Filter(function(x) x > 0, sapply(training, function(x)sum(is.na(x))) / dd[1]))
 
 with.too.many.nas <- Filter(function(x) x > 0.95, with.nas)
+sort(names(with.too.many.nas))
 
 #percentage of almost empty values
 (length(with.too.many.nas) / dd[2])*100
@@ -44,8 +45,8 @@ with.too.many.nas <- Filter(function(x) x > 0.95, with.nas)
 # now we remove the columns out of the datasets
 with.too.many.nas.indexes <- which(names(training) %in% names(with.too.many.nas))
 
-training.neat <- training[, -with.too.many.nas.indexes]
-testing.neat <- testing[, -with.too.many.nas.indexes]
+training.neat <- training[, -which(names(training) %in% names(with.too.many.nas))]
+testing.neat <- testing[, -which(names(testing) %in% names(with.too.many.nas))]
 summary(training.neat)
 dim(training.neat)
 
@@ -83,46 +84,14 @@ confusionMatrix(predict(rf.fit.cross.validation, newdata=validation.set), valida
 
 plot(varImp(rf.fit.cross.validation))
 
+test_prediction<-predict(rf.fit.cross.validation, newdata=testing.neat)
+test_prediction
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+pml_write_files(test_prediction)
 
-
-
-# let's try to get some sense of the data by exploring it a bit...
-# possible analysis are
-# kmeans => based on the number of classes? Or kind of testers
-# pca => number of significant v
-# some metrics of the data
-# see if groups have distinct means, perform t-tests
-# see if freq groups are different using Chi-sq ?
-
-# the paper is referring a specific use of adaBoost
-# it sounds good to use ada boost since if focuses on outliers and hard cases
-# why it sounds good is because the tests are only slightly different, so we can expect a lot of 
-# similarities in the observation
-
-# Since we're doing a classification, the real AdaBoost is the most approrpriate (I almost know) which
-# use tree to output probability to be of a certain class.
-library(caret)
-library(ada)
-library(rpart)
-adaboostModel <- train(y=training$classe, x=training[,-160], method="ada", type="real", data=training, iter=10, nu=0.1, maxdepth=10)
-
-adaboostModel <- train(y=training$classe, x=training[,-160], method="ada")
-
-# I could also try to mimic the exact analysis that the paper mentions, AdaBoost with C4.5!
-
-
-
-install.packages("adabag")
-library(adabag)
-library(rpart)
-v <- data.frame(classe = c("a", "b", "a"), d=c(1, 2, 1))
-b <- boosting(classe ~ . , data=v, boos=TRUE, mfinal=1)
-
-
-## rpart library should be loaded
-data(iris)
-iris.adaboost <- boosting(Species~., data=iris, boos=TRUE, mfinal=10)
-
-sub <- c(sample(1:50, 25), sample(51:100, 25), sample(101:150, 25))
-iris.bagging <- bagging(Species ~ ., data=iris[sub,], mfinal=10)
-iris.predbagging<- predict.bagging(iris.bagging, newdata=iris[-sub,])
